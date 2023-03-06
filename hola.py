237b205e4ada9ab0a6a931041ac6db406155b270
MarcosGN12 <marcosnuero11@gmail.com>
2023-03-06 18:52:59 +0100

    ejercicios

---
+++ hola.py
@@ -0,0 +1,12 @@
+#prueba de hola mundo
+
+print("Hola python")
+print('Hola python')
+
+
+print(type("Hola python")) #Tipo str (dato str significa palabras)
+print(type(5))             #Tipo int (dato int significa numero)
+print(type(5.5))           #Tipo float (dato float significa numero con decimales)
+print(type(True))          #Tipo bool (dato bool significa verdadero o falso)
+print(type(False))         #Tipo bool (dato bool significa verdadero o falso))
+print(type(5 + 3j))        #Tipo complex (dato complex significa dato complejo
---
+++ variables.py
@@ -0,0 +1,40 @@
+#variables
+
+mi_variable = "Mi string variable"
+print(mi_variable)
+
+mi_variable_numero = 5
+print(mi_variable_numero)
+
+mi_bool_variable = True
+print(mi_bool_variable)
+
+#concatenacion de variables 
+
+print(mi_variable_numero, mi_variable, mi_bool_variable)
+print("Esto deberia decirme que tienes 5 años", mi_variable_numero)
+#algunas funciones del sistema
+#len = funcion para contrar numero de caracteres
+
+print(len(mi_variable))
+
+#variables en una sola linea
+
+mi_nombre, mi_apellido, mi_edad = "Marcos", "GN", 17
+print("Mi nombre es:",mi_nombre,"Mi apellido es:",mi_apellido,"Y mi edad es",mi_edad, )
+
+#input = formulario de texto a rellenar 
+
+'''''
+cual_es_tu_nombre = input("dime tu nombre: ")
+cual_es_tu_edad = input("dime tu edad: ")
+
+print("Tu nombre es",cual_es_tu_nombre)
+print("Tu edad es",cual_es_tu_edad)
+'''''
+
+#¿Forzamos el tipo?
+address: str = "Mi direccion"
+address = 32
+address = 32.5
+print(type(address))
